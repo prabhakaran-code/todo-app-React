d2e4485d232edbdfe908e2885b48f30e5c760fc8
prabhakaran-code <prabhakaran.u@hubino.com>
2026-03-31 11:59:48 +0530

    backend

--- todo-backend/db.ts
+++ todo-backend/db.ts
@@ -1,10 +1,10 @@
 import mysql, { Connection } from "mysql2";
 
 const db: Connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "react_todo_app",
+  host: process.env.DB_HOST!,
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASSWORD!,
+  database: process.env.DB_NAME!,
 });
 
 db.connect((err: mysql.QueryError | null) => {
--- todo-backend/server.ts
+++ todo-backend/server.ts
@@ -1,11 +1,17 @@
 import express, { Application } from "express";
 import cors from "cors";
 import todoRoutes from "./routes/todoRoutes.js";
+import dotenv from "dotenv";
 
 const app: Application = express();
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: "*"
+}));
 
 app.use("/api/todos", todoRoutes);
 
