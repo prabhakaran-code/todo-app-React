d7899f6defacb5288b7333f98315463cf8d86934
prabhakaran-code <prabhakaran.u@hubino.com>
2026-03-31 14:12:23 +0530

    ..

--- todo-backend/db.ts
+++ todo-backend/db.ts
@@ -7,6 +7,7 @@ const db: Connection = mysql.createConnection({
   user: process.env.DB_USER!,
   password: process.env.DB_PASSWORD!,
   database: process.env.DB_NAME!,
+  port: Number(process.env.DB_PORT)!
 });
 
 console.log("DB_NAME:", process.env.DB_NAME);
